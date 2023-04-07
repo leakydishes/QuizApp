ccdf5e97b7572466533704d79c3f2d3033074a94
teclaire <teclairestudios@gmail.com>
2023-04-07 15:21:04 +1000

    Second Activity

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // button variable
+        Button anotherActivityButton;
+
+        // TODO link button variable with id
+        anotherActivityButton = findViewById(R.id.button);
+
+        //TODO set on button click listener
+
+        anotherActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("checking", "Button is clicked!");
+            }
+        });
+
     }
 }
